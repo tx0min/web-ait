24e0412a59bdc54de9166612426c42a0ea814cc5
Txomin <txomin@MacBook-Pro-de-Txomin.local>
2020-03-04 22:46:34 +0100

    nl2br home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 <div class="owl-carousel" id="home-slider" data-single="{{$behaviour=="random"}}">
     @foreach($slides as $slide)
         @if($slide["image"] && $slide["image"]->url)
-            
+
             <div class="home-slide" data-color="{{$slide["color"]}}" data-bg-color="{{$slide["bgcolor"] }}" >
                 {{-- @dump($slide) --}}
                 <div class="image" style="background-image:url({{$slide["image"]->url}})" ></div>
@@ -19,8 +19,8 @@
 
                 <div class="page-content">
                     <h1 class="page-title ">
-                        @if($slide["content"]) 
-                            {!! $slide["content"] !!}
+                        @if($slide["content"])
+                            {!! nl2br($slide["content"]) !!}
                         @else
                             {!! $page->post_content !!}
                         @endif
